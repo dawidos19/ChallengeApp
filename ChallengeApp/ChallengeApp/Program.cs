8d51e7e4ce8d8f8a5cf6fc57d316252e95cfb849
dawidos19 <dd.dawidos@gmail.com>
2023-05-29 21:27:48 +0200

    day4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -14,6 +14,24 @@ double myNumber2 = double.MaxValue;
 string name = "Dawid";
 string surname = "Kulon";
 string result = name + surname + myAge;
-Console.WriteLine(result);
+//Console.WriteLine(result);
 char myVar5 = 'c';
 var result2 = name.ToArray();
+
+// zmienna logiczna
+bool isActive = true;
+isActive = false;
+var isValid = 5 > 6;
+//Console.WriteLine(isValid);
+
+var number1 = 50;
+var number2 = 10;
+
+if(number1 < number2)
+{
+    Console.WriteLine("JESTEM TUTAJ W LINII 6");
+}
+else
+{
+    Console.WriteLine("JESTEM TUTAJ W LINII 10");
+}
