d14c38734ec58c6c7cbeb35d3d399a83d62d87c8
dawidos19 <dd.dawidos@gmail.com>
2023-05-29 23:28:33 +0200

    day 4 - homework

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,37 +1,28 @@
 ﻿
-// liczby calkowite
-int myAge = 120;
-int newAge = myAge + 5;
-int myVar = int.MinValue;
-uint myVar2 = uint.MaxValue;
-ulong myVar3 = ulong.MaxValue;
+string name = "Ewa";
+bool isWoman = false;
+int age = 17;
 
-// liczby zmiennoprzecinkowe
-float myNumber = float.MaxValue;
-double myNumber2 = double.MaxValue;
-
-// zmienne tekstowe
-string name = "Dawid";
-string surname = "Kulon";
-string result = name + surname + myAge;
-//Console.WriteLine(result);
-char myVar5 = 'c';
-var result2 = name.ToArray();
-
-// zmienna logiczna
-bool isActive = true;
-isActive = false;
-var isValid = 5 > 6;
-//Console.WriteLine(isValid);
-
-var number1 = 50;
-var number2 = 10;
-
-if(number1 < number2)
+if (isWoman == true)
 {
-    Console.WriteLine("JESTEM TUTAJ W LINII 6");
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else if (age >= 30)
+    {
+        Console.WriteLine("Kobieta 30 i więcej lat");
+    }
 }
-else
+else if (!(isWoman == true) && age < 18)
 {
-    Console.WriteLine("JESTEM TUTAJ W LINII 10");
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else if (isWoman == false && age >= 18)
+{
+    Console.WriteLine("Pełnoletni Mężczyzna");
 }
