9fbc6f61ac9f72aabea06194c326328f30eff12f
dawidos19 <dd.dawidos@gmail.com>
2023-05-29 21:11:34 +0200

    day4 variables

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("alea iacta est!");
+﻿
+// liczby calkowite
+int myAge = 120;
+int newAge = myAge + 5;
+int myVar = int.MinValue;
+uint myVar2 = uint.MaxValue;
+ulong myVar3 = ulong.MaxValue;
+
+// liczby zmiennoprzecinkowe
+float myNumber = float.MaxValue;
+double myNumber2 = double.MaxValue;
+
+// zmienne tekstowe
+string name = "Dawid";
+string surname = "Kulon";
+string result = name + surname + myAge;
+Console.WriteLine(result);
+char myVar5 = 'c';
+var result2 = name.ToArray();
